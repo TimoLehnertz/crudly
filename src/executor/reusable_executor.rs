8d456aebdfde31bfbae28a73df9aa2d74d53b424
Timo Lehnertz <timo.lehnertz@nerou.de>
2026-06-07 21:00:58 +0200

    Update ReusableExecutor to use Pin<Box<dyn Future>> to avoid rust limitations

--- src/executor/reusable_executor.rs
+++ src/executor/reusable_executor.rs
@@ -3,13 +3,17 @@ use sqlx::Executor;
 use sqlx::IntoArguments;
 use sqlx::Pool;
 use sqlx::query_with;
+use std::future::Future;
+use std::pin::Pin;
+
+type ReusableExecutorFuture<'a> = Pin<Box<dyn Future<Output = sqlx::Result<()>> + Send + 'a>>;
 
 pub trait ReusableExecutor<DB: Database> {
     fn execute_query_with<'a, 'q>(
         &'a mut self,
         sql: &'q str,
         arguments: DB::Arguments<'q>,
-    ) -> impl std::future::Future<Output = sqlx::Result<()>> + Send + 'a
+    ) -> ReusableExecutorFuture<'a>
     where
         'q: 'a;
 }
@@ -20,14 +24,14 @@ impl ReusableExecutor<sqlx::Sqlite> for &mut sqlx::SqliteConnection {
         &'a mut self,
         sql: &'q str,
         arguments: <sqlx::Sqlite as Database>::Arguments<'q>,
-    ) -> impl std::future::Future<Output = sqlx::Result<()>> + Send + 'a
+    ) -> ReusableExecutorFuture<'a>
     where
         'q: 'a,
     {
-        async move {
+        Box::pin(async move {
             query_with(sql, arguments).execute(&mut **self).await?;
             Ok(())
-        }
+        })
     }
 }
 
@@ -37,14 +41,14 @@ impl ReusableExecutor<sqlx::Postgres> for &mut sqlx::PgConnection {
         &'a mut self,
         sql: &'q str,
         arguments: <sqlx::Postgres as Database>::Arguments<'q>,
-    ) -> impl std::future::Future<Output = sqlx::Result<()>> + Send + 'a
+    ) -> ReusableExecutorFuture<'a>
     where
         'q: 'a,
     {
-        async move {
+        Box::pin(async move {
             query_with(sql, arguments).execute(&mut **self).await?;
             Ok(())
-        }
+        })
     }
 }
 
@@ -54,14 +58,14 @@ impl ReusableExecutor<sqlx::MySql> for &mut sqlx::MySqlConnection {
         &'a mut self,
         sql: &'q str,
         arguments: <sqlx::MySql as Database>::Arguments<'q>,
-    ) -> impl std::future::Future<Output = sqlx::Result<()>> + Send + 'a
+    ) -> ReusableExecutorFuture<'a>
     where
         'q: 'a,
     {
-        async move {
+        Box::pin(async move {
             query_with(sql, arguments).execute(&mut **self).await?;
             Ok(())
-        }
+        })
     }
 }
 
@@ -70,16 +74,18 @@ where
     for<'e> <DB as Database>::Arguments<'e>: IntoArguments<'e, DB>,
     for<'e> &'e mut DB::Connection: Executor<'e, Database = DB>,
 {
-    async fn execute_query_with<'a, 'q>(
+    fn execute_query_with<'a, 'q>(
         &'a mut self,
         sql: &'q str,
         arguments: DB::Arguments<'q>,
-    ) -> sqlx::Result<()>
+    ) -> ReusableExecutorFuture<'a>
     where
         'q: 'a,
     {
-        query_with(sql, arguments).execute(&*self).await?;
-        Ok(())
+        Box::pin(async move {
+            query_with(sql, arguments).execute(&*self).await?;
+            Ok(())
+        })
     }
 }
 
@@ -88,16 +94,18 @@ where
     for<'e> <DB as Database>::Arguments<'e>: IntoArguments<'e, DB>,
     for<'e> &'e mut DB::Connection: Executor<'e, Database = DB>,
 {
-    async fn execute_query_with<'a, 'q>(
+    fn execute_query_with<'a, 'q>(
         &'a mut self,
         sql: &'q str,
         arguments: DB::Arguments<'q>,
-    ) -> sqlx::Result<()>
+    ) -> ReusableExecutorFuture<'a>
     where
         'q: 'a,
     {
-        query_with(sql, arguments).execute(*self).await?;
-        Ok(())
+        Box::pin(async move {
+            query_with(sql, arguments).execute(*self).await?;
+            Ok(())
+        })
     }
 }
 
@@ -106,15 +114,17 @@ where
     for<'e> <DB as Database>::Arguments<'e>: IntoArguments<'e, DB>,
     for<'e> &'e mut DB::Connection: Executor<'e, Database = DB>,
 {
-    async fn execute_query_with<'a, 'q>(
+    fn execute_query_with<'a, 'q>(
         &'a mut self,
         sql: &'q str,
         arguments: DB::Arguments<'q>,
-    ) -> sqlx::Result<()>
+    ) -> ReusableExecutorFuture<'a>
     where
         'q: 'a,
     {
-        query_with(sql, arguments).execute(&**self).await?;
-        Ok(())
+        Box::pin(async move {
+            query_with(sql, arguments).execute(&**self).await?;
+            Ok(())
+        })
     }
 }
